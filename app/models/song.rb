c1c846c0b5837498a16da3fb17a19304ccb78c48
remote-torrent-6747 <abbyc@sky.com>
2021-03-28 09:35:10 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    if artists.exclude?("Drake")
+    if artists.any? {|artist| artist.name == "Drake"}
       drake = Artist.create(name: "Drake")
     end
     created_song = Song.create(name: self.song.name)
